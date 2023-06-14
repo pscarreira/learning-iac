e95ae9ca52596ac8ad57385f1aa797343b066787
pscarreira <devpaulinho@gmail.com>
2023-06-14 17:25:33 -0300

    Adicionando arquivos Ansible

--- main.tf
+++ main.tf
@@ -15,19 +15,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-053b0d53c279acc90"
-  instance_type = "t2.micro"
-  key_name      = "terraform-aws"
-  user_data     = <<-EOF
-  #!/bin/bash
-  cd /home/ubuntu
-  echo "<h1>Feito com Terraform</h1>" > index.html
-  nohup busybox httpd -f -p 8080 &
-  EOF
-
+  ami                         = "ami-053b0d53c279acc90"
+  instance_type               = "t2.micro"
+  key_name                    = "terraform-aws"
   user_data_replace_on_change = true
   tags = {
     Name = "Teste AWS"
   }
   security_groups = ["acesso-ssh", "acesso-total"]
 }
+
+
